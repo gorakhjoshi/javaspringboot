5ed9570b8773a00349b03fba54f41ae32a55cd97
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2025-01-15 18:05:48 -0500

    07-spring-boot-rest-crud-employee-list-employees

--- src/test/java/com/luv2code/springboot/cruddemo/CruddemoApplicationTests.java
+++ src/test/java/com/luv2code/springboot/cruddemo/CruddemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.luv2code.demo;
+package com.luv2code.springboot.cruddemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class CruddemoApplicationTests {
 
 	@Test
 	void contextLoads() {
