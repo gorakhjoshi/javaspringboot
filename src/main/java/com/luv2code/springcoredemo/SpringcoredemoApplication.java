4b2703bc63e959ccebe1c9a1564871d1c978fe7f
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2025-01-15 17:51:29 -0500

    01-constructor-injection

--- src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
@@ -1,13 +1,13 @@
-package com.luv2code.springboot.demo.mycoolapp;
+package com.luv2code.springcoredemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MycoolappApplication {
+public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MycoolappApplication.class, args);
+		SpringApplication.run(SpringcoredemoApplication.class, args);
 	}
 
 }
--- src/test/java/com/luv2code/springcoredemo/SpringcoredemoApplicationTests.java
+++ src/test/java/com/luv2code/springcoredemo/SpringcoredemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.luv2code.springboot.demo.mycoolapp;
+package com.luv2code.springcoredemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MycoolappApplicationTests {
+class SpringcoredemoApplicationTests {
 
 	@Test
 	void contextLoads() {
