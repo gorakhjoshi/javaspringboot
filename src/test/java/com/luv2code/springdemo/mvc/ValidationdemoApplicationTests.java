10d5ba471f0f161d7d6b746a0ddc02c7d87d1cd9
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2025-01-16 23:18:13 -0500

    14-validationdemo-required-fields

--- src/test/java/com/luv2code/springdemo/mvc/ValidationdemoApplicationTests.java
+++ src/test/java/com/luv2code/springdemo/mvc/ValidationdemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.luv2code.springboot.thymeleafdemo;
+package com.luv2code.springdemo.mvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ThymeleafdemoApplicationTests {
+class ValidationdemoApplicationTests {
 
 	@Test
 	void contextLoads() {
